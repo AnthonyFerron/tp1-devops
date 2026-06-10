0260d007ca02b92fd671effb6d31b353e856aeff
AnthonyFerron <anthony.ferron74@gmail.com>
2026-06-10 19:05:28 +0200

    update message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Bonjour DevOps !");
+  res.send("Bonjour DevOps 2026 !");
 });
 
 // Render fournit le port via la variable d'environnement PORT.
